e67497a56563377eb84ed5a0c532b9d63f7487f5
Tom <tom@tom-GX623-PX603.(none)>
2013-04-27 23:41:01 +0200

    Project start

--- AVG-SRU/gen/com/pda_sru/R.java
+++ AVG-SRU/gen/com/pda_sru/R.java
@@ -10,6 +10,10 @@ package com.pda_sru;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int cyan=0x7f040001;
+        public static final int dim_gray=0x7f040000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -17,25 +21,40 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int button_disabled=0x7f020000;
+        public static final int button_pressed=0x7f020001;
+        public static final int button_selector=0x7f020002;
+        public static final int button_unpressed=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_change_group=0x7f090003;
+        public static final int action_create_group=0x7f090000;
+        public static final int action_edit_profile=0x7f090004;
+        public static final int action_join_group=0x7f090001;
+        public static final int action_view_groups=0x7f090002;
+        public static final int action_view_members=0x7f090005;
     }
     public static final class layout {
         public static final int layout_home=0x7f030000;
     }
     public static final class menu {
-        public static final int home=0x7f070000;
+        public static final int menu=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int app_name=0x7f060000;
+        public static final int en_action_change_group=0x7f060004;
+        /**  Menu items 
+         */
+        public static final int en_action_create_group=0x7f060001;
+        public static final int en_action_edit_profile=0x7f060005;
+        public static final int en_action_join_group=0x7f060002;
+        public static final int en_action_view_groups=0x7f060003;
+        public static final int en_action_view_members=0x7f060006;
     }
     public static final class style {
         /** 
@@ -59,10 +78,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- AVG-SRU/src/com/pda_sru/Home.java
+++ AVG-SRU/src/com/pda_sru/Home.java
@@ -3,20 +3,74 @@ package com.pda_sru;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class Home extends Activity {
+public class Home extends Activity implements OnClickListener {
 
+	/**
+	 * Called when Activity is created. Should initiate all layout object here.
+	 */
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.layout_home);
 	}
 
+	/**
+	 * Initiates the menu for this Activity
+	 */
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.home, menu);
+		getMenuInflater().inflate(R.menu.menu, menu);
 		return true;
+	}
+	
+	/**
+	 * Event Handling for Individual menu items
+	 */
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		int id = item.getItemId();
+		
+		switch(id) {
+		case R.id.action_change_group:
+			
+			return true;
+		case R.id.action_create_group:
+			
+			return true;
+		case R.id.action_edit_profile:
+			
+			return true;
+		case R.id.action_join_group:
+			
+			return true;
+		case R.id.action_view_groups:
+			
+			return true;
+		case R.id.action_view_members:
+			
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+		
+	}
+
+	/**
+	 * This Activity onClick method should implement onClickListeners
+	 * for all components that should respond to a click
+	 */
+	@Override
+	public void onClick(View v) {
+		int id = v.getId();
+		
+		switch(id) {
+		
+		}
+		
 	}
 
 }
